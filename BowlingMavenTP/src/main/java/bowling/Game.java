9b5246afad0753bd83a156a67316d05fe1c0e70d
Apakoh <Spard@DESKTOP-505V0HE>
2018-09-26 16:02:45 +0200

    Interface MultiPlayerGame Implementation

--- BowlingMavenTP/src/main/java/bowling/Game.java
+++ BowlingMavenTP/src/main/java/bowling/Game.java
@@ -10,6 +10,21 @@ package bowling;
  *
  * @author Spard
  */
-public class Game {
+public class Game implements MultiPlayerGame {
+
+    @Override
+    public String startNewGame(String[] playerName) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String lancer(int nombreDeQuillesAbattues) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int scoreFor(String playerName) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
